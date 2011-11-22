00f377e16b140d77a53296621f65d08372806be5
Chris Boertien <chris.boertien@gmail.com>
2011-11-22 08:18:16 -0700

    add spree initializer
    Signed-off-by: Chris Boertien <chris.boertien@gmail.com>

---
+++ config/initializers/spree.rb
@@ -0,0 +1,5 @@
+Spree::Config.set(:products_per_page => 24)
+Spree::Config.set(:orders_per_page => 35)
+Spree::Config.set(:admin_products_per_page => 20)
+Spree::Config.set(:allow_backorders => false)
+I18n.locale = :en
