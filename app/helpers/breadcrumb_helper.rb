bb850cc6a4f1ea551e3734af0eabb8c2eed6caa7
Chris Boertien <chris.boertien@gmail.com>
2011-11-22 09:24:53 -0700

    Remove taxon helper override and move custom code into it's own helper
    Signed-off-by: Chris Boertien <chris.boertien@gmail.com>

--- app/helpers/breadcrumb_helper.rb
+++ app/helpers/breadcrumb_helper.rb
@@ -1,4 +1,4 @@
-module TaxonsHelper
+module BreadcrumbHelper
   def breadcrumbs(taxon, separator="&nbsp;&raquo;&nbsp;")
     return "" if current_page?("/")
     separator = raw(separator)
@@ -12,21 +12,6 @@ module TaxonsHelper
     end 
     crumb_list = content_tag(:ul, raw(crumbs.flatten.map{|li| li.mb_chars}.join))
     content_tag(:div, crumb_list + tag(:br, {:class => 'clear'}, false, true), :class => 'breadcrumbs')
-  end 
-
-  # Retrieves the collection of products to display when "previewing" a taxon.  This is abstracted into a helper so
-  # that we can use configurations as well as make it easier for end users to override this determination.  One idea is
-  # to show the most popular products for a particular taxon (that is an exercise left to the developer.)
-  def taxon_preview(taxon, max=4)
-    products = taxon.active_products.limit(max)
-    if (products.size < max) && Spree::Config[:show_descendents]
-      taxon.descendants.each do |taxon|
-        to_get = max - products.length
-        products += taxon.active_products.limit(to_get)
-        break if products.size >= max 
-      end 
-    end 
-    products
   end 
 end
 
