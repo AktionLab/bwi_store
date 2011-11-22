b938f765812d5d0510e563d4527f00724e2173fc
Chris Boertien <chris.boertien@gmail.com>
2011-11-22 08:19:12 -0700

    add shipment/tax_charge overrides to order
    Signed-off-by: Chris Boertien <chris.boertien@gmail.com>

--- app/models/order_decorator.rb
+++ app/models/order_decorator.rb
@@ -7,5 +7,21 @@ Order.class_eval do
 
     save(:validate => false)
   end
+
+  def create_shipment!
+    shipping_method(true)
+    if shipment.present?
+      shipment.update_attributes(:shipping_method => shipping_method)
+    else
+      self.shipments << Shipment.create(order => self, :shipping_method => shipping_method, :address => self.ship_address)
+    end
+    self.create_tax_charge!
+  end
+
+  def create_tax_charge!
+    adjustments.tax.each {|e| e.destroy}
+    return if self.user && self.user.user_group && self.user.user_group == UserGroup.find_by_name("Wholesale")
+    TaxRate.match(ship_address).each {|r| r.create_adjustment(I18n.t(:tax), self, self, true)}
+  end
 end
 
