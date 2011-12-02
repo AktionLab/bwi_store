0180b3cf894f8b719be0ed955bebe9cf95fe30a6
Chris Boertien <chris.boertien@gmail.com>
2011-12-01 19:08:48 -0700

    or better yet, just ignore the exception
    Signed-off-by: Chris Boertien <chris.boertien@gmail.com>

--- config/initializers/spree.rb
+++ config/initializers/spree.rb
@@ -1,5 +1,8 @@
-#Spree::Config.set(:products_per_page => 24)
-#Spree::Config.set(:orders_per_page => 35)
-#Spree::Config.set(:admin_products_per_page => 20)
-#Spree::Config.set(:allow_backorders => false)
-#I18n.locale = :en
+begin
+  Spree::Config.set(:products_per_page => 24)
+  Spree::Config.set(:orders_per_page => 35)
+  Spree::Config.set(:admin_products_per_page => 20)
+  Spree::Config.set(:allow_backorders => false)
+rescue
+end
+I18n.locale = :en
