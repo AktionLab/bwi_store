aa9a8e4378772239a9166d8f7086201f47ba18f6
Chris Boertien <chris.boertien@bz-labs.com>
2011-12-12 09:26:35 -0700

    add flexi rate admin routes
    Signed-off-by: Chris Boertien <chris.boertien@bz-labs.com>

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 BwiStore::Application.routes.draw do
   root :to => 'taxons#show', :id => 'brand'
   namespace :admin do
+    resources :flexi_shipping_rates
     resources :user_groups do
       member do
         get :pricing, :as => 'user_group'
