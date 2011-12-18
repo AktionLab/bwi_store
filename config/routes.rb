7a078e84e7515cec3b129dd9186743e3c49510b5
Chris Boertien <chris.boertien@bz-labs.com>
2011-12-17 21:10:44 -0700

    change home page to about, closes #4
    Signed-off-by: Chris Boertien <chris.boertien@bz-labs.com>

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 BwiStore::Application.routes.draw do
-  root :to => 'taxons#show', :id => 'brand'
+  root :to => redirect('/about')
+  #root :to => 'taxons#show', :id => 'brand'
   namespace :admin do
     resources :flexi_shipping_rates
     resources :user_groups do
